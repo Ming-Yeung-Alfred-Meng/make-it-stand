dbe63e5b5437c707e881cd239a002e09446e2313
Ming-Yeung-Alfred-Meng <alfred845301120@gmail.com>
2022-11-01 11:43:45 -0400

    Implemented angle_defect.cpp.

--- src/angle_defect.cpp
+++ src/angle_defect.cpp
@@ -1,4 +1,6 @@
 #include "../include/angle_defect.h"
+#include "internal_angles.h"
+#include <igl/squared_edge_lengths.h>
 
 void angle_defect(
   const Eigen::MatrixXd & V,
@@ -6,4 +8,17 @@ void angle_defect(
   Eigen::VectorXd & D)
 {
   D = Eigen::VectorXd::Zero(V.rows());
+  Eigen::MatrixXd l_sqr;
+  Eigen::MatrixXd angles;
+
+  igl::squared_edge_lengths(V, F, l_sqr);
+  internal_angles(l_sqr, angles);
+
+  for (int f = 0; f < F.rows(); ++f) {
+    for (int v = 0; v < 3; ++v) {
+      D(F(f, v)) += angles(f, v);
+    }
+  }
+
+  D =  Eigen::VectorXd::Constant(V.rows(), 2 * acos(-1.0)) - D;
 }
