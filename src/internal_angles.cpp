7a325f9410a2982fe38257c8a9a001d8d359e552
Ming-Yeung-Alfred-Meng <alfred845301120@gmail.com>
2022-10-31 23:07:47 -0400

    Implemented internal_angles.

--- src/internal_angles.cpp
+++ src/internal_angles.cpp
@@ -1,8 +1,21 @@
 #include "../include/internal_angles.h"
+#include <cmath>
 
 void internal_angles(
   const Eigen::MatrixXd & l_sqr,
   Eigen::MatrixXd & A)
 {
-  // Add with your code
+  A.resizeLike(l_sqr);
+
+  Eigen::MatrixXd l = l_sqr.cwiseSqrt();
+
+  for (int i = 0; i < l_sqr.rows(); ++i) {
+    for (int j = 0; j < 3; ++j) {
+      int left = (j + 1) % 3;
+      int right = (j + 2) % 3;
+      A(i, j) = acos(
+                               (l_sqr(i, j) - l_sqr(i, left) - l_sqr(i, right))
+                               / (-2 * l(i, left) * l(i, right)));
+    }
+  }
 }
