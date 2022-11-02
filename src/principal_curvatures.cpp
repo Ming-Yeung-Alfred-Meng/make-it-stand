5dcfe38fa656b1b5e898b0b598b1f5eec180ddfd
Ming-Yeung-Alfred-Meng <alfred845301120@gmail.com>
2022-11-02 19:51:00 -0400

    Implemented principal_curvatures.cpp.

--- src/principal_curvatures.cpp
+++ src/principal_curvatures.cpp
@@ -1,6 +1,8 @@
 #include "../include/principal_curvatures.h"
 #include <igl/adjacency_list.h>
 #include <unordered_set>
+#include <cmath>
+#include <igl/sort.h>
 
 
 void set_insert(
@@ -47,8 +49,9 @@ void build_tworing(
   tworing.rowwise() -= V.row(v);
 }
 
-
-void change_basis(
+// may need to change the paramerter name of "change of basis", it is confusing, since it is the inverse
+// of what it is supposed to mean. May be add a bool parameter that indicate whether it is the inverse or not.
+void world_to_local(
   const Eigen::MatrixXd & change_of_basis,
   const Eigen::MatrixXd & A,
   Eigen::MatrixXd & B)
@@ -57,26 +60,51 @@ void change_basis(
   B = A * change_of_basis;
 }
 
-
+// coeff: k x 5
+// k is the number of rows in tworing_local
+// tworing_local: k x 3 (u, v, w)
 void build_quadratic_coeff(
   const Eigen::MatrixXd & tworing_local,
   Eigen::MatrixXd & coeff)
 {
+  // resize coeff to k x 5
+  // loop each row of tworing_local,
+  // use the first two column of tworing_local to fill in coeff
 
+  coeff.resize(tworing_local.rows(), 5);
+
+  for (int i = 0; i < tworing_local.rows(); ++i) {
+    coeff(i, 0) = tworing_local(i, 0);
+    coeff(i, 1) = tworing_local(i, 1);
+    coeff(i, 2) = pow(tworing_local(i, 0), 2);
+    coeff(i, 3) = tworing_local(i, 0) * tworing_local(i, 1);
+    coeff(i, 4) = pow(tworing_local(i, 1), 2);
+  }
 }
 
+
+// a: 5 vector
+// S_left: 2 x 2 matrix
 void build_S_left(
   const Eigen::VectorXd & a,
   Eigen::Matrix2d & S_left)
 {
-
+  double denominator = sqrt(pow(a[0], 2) + pow(a[1], 2) + 1);
+  S_left(0, 0) =  2 * a[2] / denominator;
+  S_left(0, 1) = a[3] / denominator;
+  S_left(1, 0) = S_left(0, 1);
+  S_left(1, 1) = 2 * a[4] / denominator;
 }
+
 
 void build_S_right(
   const Eigen::VectorXd & a,
   Eigen::Matrix2d & S_right)
 {
-
+  S_right(0, 0) = 1 + pow(a[0], 2);
+  S_right(0, 1) = a[0] * a[1];
+  S_right(1, 0) = S_right(0, 1);
+  S_right(1, 1) = 1 + pow(a[1], 2);
 }
 
 
@@ -84,7 +112,8 @@ void build_shape_operator(
   const Eigen::MatrixXd & V,
   const std::vector<std::vector<int>> & adjacency_l,
   const int v,
-  Eigen::Matrix2d & S)
+  Eigen::Matrix2d & S,
+  Eigen::Matrix3d & r_sgl_vectors)
 {
   // i.e. find k x 3 tworing,
   // svd decompose tworing
@@ -100,11 +129,9 @@ void build_shape_operator(
 
   build_tworing(V, adjacency_l, v, tworing);
 
-  Eigen::JacobiSVD<Eigen::MatrixXd> svd(tworing, Eigen::ComputeThinV);
-  //  svd.singularValues(); //decreasing order.
+  Eigen::JacobiSVD<Eigen::MatrixXd> svd(tworing, Eigen::ComputeThinV);  // svd.singularValues() sorted in decreasing order.
 
-//  change_basis(svd.matrixV().inverse(), tworing, tworing_local);
-  change_basis(svd.matrixV(), tworing, tworing_local);
+  world_to_local(svd.matrixV(), tworing, tworing_local);
 
   build_quadratic_coeff(tworing_local, coeff);
 
@@ -116,6 +143,16 @@ void build_shape_operator(
   build_S_right(a, S_right);
 
   S = - (S_left * S_right.inverse());
+  r_sgl_vectors = svd.matrixV();
+}
+
+
+void local_to_world(
+  const Eigen::Matrix<double, 3, 2> & change_of_basis,
+  const Eigen::Matrix2d & local,
+  Eigen::Matrix<double, 3, 2> & world)
+{
+  world = change_of_basis * local;
 }
 
 
@@ -127,27 +164,37 @@ void principal_curvatures(
   Eigen::VectorXd & K1,
   Eigen::VectorXd & K2)
 {
-  // for every vertex
-  // compute shape operator:
-  // i.e. find k x 3 neighbourhood,
-  // svd decompose neighbourhood
-  // fit curve
-  // build shape operator
-  // eigen decompose shape operator
-  // shift points
   Eigen::Matrix2d S;
   std::vector<std::vector<int>> adjacency_l;
+  Eigen::Vector2d curvatures;
+  Eigen::Vector2i directions_i;
+  Eigen::Matrix<double, 3, 2> directions_world;
+  Eigen::Matrix3d r_sgl_vectors;
+
+  K1.resize(V.rows());
+  K2.resize(V.rows());
+  D1.resize(V.rows(), 3);
+  D2.resize(V.rows(), 3);
 
   igl::adjacency_list(F, adjacency_l, false);
 
   for (int v = 0; v < V.rows(); ++v) {
-    build_shape_operator(V, adjacency_l, v, S);
-//    eigen_decompose();
-//    map_to_world();
-  }
+    build_shape_operator(V, adjacency_l, v, S, r_sgl_vectors);
 
-  K1 = Eigen::VectorXd::Zero(V.rows());
-  K2 = Eigen::VectorXd::Zero(V.rows());
-  D1 = Eigen::MatrixXd::Zero(V.rows(),3);
-  D2 = Eigen::MatrixXd::Zero(V.rows(),3);
+    // what if eigenvalues are complex.
+    Eigen::EigenSolver<Eigen::Matrix2d> eigensolver(S);
+    if (eigensolver.info() != Eigen::Success) abort();
+
+    igl::sort(eigensolver.eigenvalues().real(), 1, false, curvatures, directions_i);
+
+    local_to_world(r_sgl_vectors.block<3, 2>(0, 0), eigensolver.eigenvectors().real(), directions_world);
+
+    K1(v) = curvatures(0); // max
+    K2(v) = curvatures(1);
+
+    for (int i = 0; i < 3; ++i) {
+      D1(v, i) = directions_world(i, directions_i(0));
+      D2(v, i) = directions_world(i, directions_i(1));
+    }
+  }
 }
