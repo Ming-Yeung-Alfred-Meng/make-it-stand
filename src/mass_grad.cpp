70d36092e79bc8729323a460b78718033239a005
Ming-Yeung-Alfred-Meng <alfred845301120@gmail.com>
2022-12-03 19:41:37 -0500

    mass_grad.cpp

--- src/mass_grad.cpp
+++ src/mass_grad.cpp
@@ -8,22 +8,19 @@ void mass_grad(
   grad.resize(V.rows());
   grad.setZero();
 
-  // go over every face
-  //  for each vertex, take the derivative of the face's contribution wrt that vertex
-  //  add the derivative wrt each coordinate of the vertex into the appropriate slot of grad
-
-  // and use mod to calculate indices into F.
   double grad_x;
 
   for (int i = 0; i < F.rows(); ++i) {
-    grad_x = (V.row(F(i, 1)) - V.row(F(i, 0))).cross(
-       V.row(F(i, 2)) - V.row(F(i, 0)))(0);
+    grad_x = (V.row(F(i, 1)) - V.row(F(i, 0))).cross(V.row(F(i, 2)) - V.row(F(i, 0)))(0);
 
     for (int j = 0; j < 3; ++j) {
       grad(3 * F(i, j)) += grad_x;
-      grad(3 * F(i, j) + 1) += () * ();
-      grad(3 * F(i, j) + 2) += () * ();
+
+      grad(3 * F(i, j) + 1) += (V(F(i, (j + 1) % 3), 2) - V(F(i, (j + 2) % 3), 2))
+                               * (V(F(i, j), 0) + V(F(i, (j + 1) % 3), 0) + V(F(i, (j + 2) % 3), 0));
+
+      grad(3 * F(i, j) + 2) += (V(F(i, (j + 2) % 3), 1) - V(F(i, (j + 1) % 3), 1))
+                               * (V(F(i, j), 0) + V(F(i, (j + 1) % 3), 0) + V(F(i, (j + 2) % 3), 0));
     }
   }
-
 }
