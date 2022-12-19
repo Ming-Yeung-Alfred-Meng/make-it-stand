f7451f01f95acf4dcb7633ff16441cbec26eb04a
Ming-Yeung-Alfred-Meng <alfred845301120@gmail.com>
2022-12-19 07:10:58 -0500

    Added a test of center_of_mass() on a cube.

---
+++ test/center_of_mass_test.cpp
@@ -0,0 +1,27 @@
+#include "gtest/gtest.h"
+#include "igl/centroid.h"
+#include "igl/readOBJ.h"
+#include "center_of_mass.h"
+
+
+// Center of mass of a cube with center (0, 0, 0)
+TEST(ExampleTest, ExampleTestName) {
+  Eigen::MatrixXd V;
+  Eigen::MatrixXi F;
+  igl::readOBJ("../../data/cube.obj", V, F);
+
+  assert(V.cols() == 3);
+  assert(F.cols() == 3);
+
+  Eigen::Vector3d CoM;
+  Eigen::Vector3d centroid;
+  double mass;
+  double volume;
+  const double density = 3;
+
+  center_of_mass(V, F, density, CoM, mass);
+  igl::centroid(V, F, centroid, volume);
+
+  EXPECT_TRUE(CoM.isApprox(centroid));
+  EXPECT_DOUBLE_EQ(mass, density * volume);
+}
