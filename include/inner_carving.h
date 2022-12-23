ae983b999fe4177023b66ec717a77129a45293e4
Ming-Yeung-Alfred-Meng <alfred845301120@gmail.com>
2022-12-23 14:45:58 -0500

    Added declaration of helper functions in inner_carving.cpp to inner_carving.h.

--- include/inner_carving.h
+++ include/inner_carving.h
@@ -6,10 +6,17 @@ void inner_carving(
   const Eigen::MatrixXd &MoV,
   const Eigen::MatrixXi &MoF,
   const Eigen::Vector3d &contact,
-  const int voxel_scale,
-  const int min_carve,
-  const double thickness,
-  const double density,
+  int voxel_scale,
+  int min_carve,
+  double thickness,
+  double density,
   Eigen::MatrixXd &MiV,
   Eigen::MatrixXi &MiF);
+
+double carving_energy(const Eigen::Vector3d &CoM, const Eigen::Vector3d &contact);
+void update_center_of_mass(const Eigen::Vector3d &center, double length, double old_mass, double new_mass, double density, Eigen::Vector3d &CoM);
+double reduce_mass_by_a_voxel(double old_mass, double density, double length);
+void build_in_out(const std::vector<int>::iterator &begin, const std::vector<int>::iterator &end, long size, int in_out[]);
+std::function<bool (int, int)> generate_comp(const Eigen::MatrixXd &grid, const Eigen::Vector3d &contact, const Eigen::Vector3d &CoM);
+double distance_from_plane(const Eigen::Vector3d &query, const Eigen::Vector3d &contact, const Eigen::Vector3d &CoM);
 #endif //CURVATURE_INNER_CARVING_H
