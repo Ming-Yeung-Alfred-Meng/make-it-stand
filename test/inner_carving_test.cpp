80aba89df2dd4c65404f3e961cfce90fcd05465b
Ming-Yeung-Alfred-Meng <alfred845301120@gmail.com>
2022-12-23 15:16:32 -0500

    Refined the tests for the lambda comparator used in inner_carving.cpp.

--- test/inner_carving_test.cpp
+++ test/inner_carving_test.cpp
@@ -31,13 +31,12 @@ TEST_F(DistanceFromPlaneTest, NegativeDistance) {
 }
 
 
-// create a function that takes in two indices of queries, and return whether
-// the first point into queries is closer to the plane than the second
-// initialize queries with some interesting points, each pair represent a test case
-//
-class GenerateCompTest : public ::testing::Test {
+class SortComparatorTest : public ::testing::Test {
 protected:
     void SetUp() override {
+      contact << 1, 1, 0;
+      CoM << 2, 2, 0;
+
       queries.resize(6, 3);
       queries << 1, 2, 0,
                  2, 2, 1,
@@ -46,117 +45,57 @@ protected:
                  1, 0, 4,
                  0, 0, 5;
 
-      Eigen::Vector3d contact(1, 1, 0);
-      Eigen::Vector3d CoM(2, 2, 0);
-      comp = generate_comp(queries, contact, CoM);
+      comp = [&](int i, int j)
+      {
+          return distance_from_plane(queries.row(i), contact, CoM)
+                 > distance_from_plane(queries.row(j), contact, CoM);
+      };
+
+      indices.resize(6);
+      std::iota(indices.begin(), indices.end(), 0);
     }
 
+    Eigen::Vector3d contact;
+    Eigen::Vector3d CoM;
     Eigen::MatrixXd queries;
     std::function<bool (int, int)> comp;
+    std::vector<int> indices;
 };
 
 // test for both in front of and behind plane and one in front and one behind
-TEST_F(GenerateCompTest, SameDistance) {
+TEST_F(SortComparatorTest, SameDistance) {
+  // both positive distance
   ASSERT_FALSE(comp(0, 2));
   EXPECT_FALSE(comp(2, 0));
 
+  // both negative distance
   ASSERT_FALSE(comp(3, 4));
   EXPECT_FALSE(comp(4, 3));
 }
 
-TEST_F(GenerateCompTest, FirstGreaterThanSecond) {
-  // both in front
+TEST_F(SortComparatorTest, FirstGreaterThanSecond) {
+  // both positive distance
   EXPECT_TRUE(comp(1, 0));
 
-  // both behind
+  // both negative distance
   EXPECT_TRUE(comp(4, 5));
 
-  // one in the front, one behind
-  EXPECT_TRUE(comp(2, 3));
-}
-
-TEST_F(GenerateCompTest, SecondGreaterThanFirst) {
-  // both in front
-  EXPECT_TRUE(comp(1, 2));
-
-  // both behind
-  EXPECT_TRUE(comp(5, 3));
-
-  // one in the front, one behind
-  EXPECT_TRUE(comp(5, 1));
-}
-
-
-// Create a lambda expression that  takes in two indices of queries, and return whether
-// the first point into queries is closer to the plane than the second
-// initialize queries with some interesting points, each pair represent a test case
-//
-class GenerateCompTestBeta : public ::testing::Test {
-protected:
-    void SetUp() override {
-      queries.resize(6, 3);
-      queries << 1, 2, 0,
-                2, 2, 1,
-                2, 1, 2,
-                0, 1, 3,
-                1, 0, 4,
-                0, 0, 5;
-
-      comp = generate_comp(queries, Eigen::Vector3d(1, 1, 0), Eigen::Vector3d(2, 2, 0));
-    }
-
-    Eigen::MatrixXd queries;
-    std::function<bool (int, int)> comp;
-};
-
-
-TEST(name, name) {
-  Eigen::Vector3d contact = Eigen::Vector3d(1, 1, 0);
-  Eigen::Vector3d CoM = Eigen::Vector3d(2, 2, 0);
-  Eigen::MatrixXd grid(6, 3);
-  grid << 1, 2, 0,
-    2, 2, 1,
-    2, 1, 2,
-    0, 1, 3,
-    1, 0, 4,
-    0, 0, 5;
-
-  std::function<bool (int, int)> comp = [&](int i, int j)
-  {
-      double d1 = distance_from_plane(grid.row(i), contact, CoM);
-      double d2 = distance_from_plane(grid.row(j), contact, CoM);
-      return d1 > d2;
-  };
-
-  ASSERT_FALSE(comp(0, 2));
-  EXPECT_FALSE(comp(2, 0));
-
-  ASSERT_FALSE(comp(3, 4));
-  EXPECT_FALSE(comp(4, 3));
-
-//  comp(4, 5);
-//  comp(0, 1);
-//  comp(2, 4);
-  // both in front
-  EXPECT_TRUE(comp(1, 0));
-
-  // both behind
-  EXPECT_TRUE(comp(4, 5));
-
-  // one in the front, one behind
+  // one positive and one negative
   EXPECT_TRUE(comp(2, 4));
+}
 
-
+TEST_F(SortComparatorTest, SecondGreaterThanFirst) {
+  // both positive distance
   EXPECT_FALSE(comp(2, 1));
 
-  // both behind
+  // both negative distance
   EXPECT_FALSE(comp(5, 3));
 
-  // one in the front, one behind
+  // one positive and one negative
   EXPECT_FALSE(comp(5, 1));
+}
 
-  std::vector<int>indices(6);
-  std::iota(indices.begin(), indices.end(), 0);
+TEST_F(SortComparatorTest, Sort) {
   std::sort(indices.begin(), indices.end(), comp);
 
   ASSERT_TRUE(indices[0] == 1);
@@ -165,38 +104,6 @@ TEST(name, name) {
   ASSERT_TRUE(indices[3] == 3 || indices[3] == 4);
   ASSERT_TRUE(indices[4] == 3 || indices[4] == 4);
   ASSERT_TRUE(indices[5] == 5);
-}
-
-
-// test for both in front of and behind plane and one in front and one behind
-TEST_F(GenerateCompTestBeta, SameDistance) {
-  ASSERT_FALSE(comp(0, 2));
-  EXPECT_FALSE(comp(2, 0));
-
-  ASSERT_FALSE(comp(3, 4));
-  EXPECT_FALSE(comp(4, 3));
-}
-
-TEST_F(GenerateCompTestBeta, FirstCloser) {
-  // both in front
-  EXPECT_FALSE(comp(0, 1));
-
-  // both behind
-  EXPECT_FALSE(comp(4, 5));
-
-  // one in the front, one behind
-  EXPECT_FALSE(comp(2, 4));
-}
-
-TEST_F(GenerateCompTestBeta, SecondCloser) {
-  // both in front
-  EXPECT_TRUE(comp(1, 2));
-
-  // both behind
-  EXPECT_TRUE(comp(5, 3));
-
-  // one in the front, one behind
-  EXPECT_TRUE(comp(5, 1));
 }
 
 
